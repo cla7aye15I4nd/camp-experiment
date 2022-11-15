7bbd0ab06ab505101305142b6d8ec598d985f03e
dataisland <dataisland@outlook.com>
2022-11-14 20:49:01 -0600

    Add test.sh

---
+++ nginx/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd nginx-1.22.1
+
+sudo ./asan/sbin/nginx
+ab -n 10000000 -c 100 http://0.0.0.0/index.html > ../asan.txt
+sudo ./asan/sbin/nginx -s stop
+sleep 1
+
+sudo ./native/sbin/nginx
+ab -n 10000000 -c 100 http://0.0.0.0/index.html > ../native.txt
+sudo ./native/sbin/nginx -s stop
+sleep 1
+
+sudo LD_LIBRARY_PATH=/home/moe/violet/build/src/safe_tcmalloc/tcmalloc ./camp/sbin/nginx
+ab -n 10000000 -c 100 http://0.0.0.0/index.html > ../camp.txt
+sudo LD_LIBRARY_PATH=/home/moe/violet/build/src/safe_tcmalloc/tcmalloc ./camp/sbin/nginx -s stop
