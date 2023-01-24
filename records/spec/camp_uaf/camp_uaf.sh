fc07f68fc0f4e7bd660f97d7394b39ccde78725a
dataisland <dataisland@outlook.com>
2023-01-23 20:31:21 -0600

    Add camp uaf test result

---
+++ records/spec/camp_uaf/camp_uaf.sh
@@ -0,0 +1,4 @@
+./int_camp_uaf_2017_test.sh | tee camp_uaf_2017_int.txt
+./fp_camp_uaf_2017_test.sh | tee camp_uaf_2017_fp.txt
+./all_c_camp_uaf_2006_test.sh | tee camp_uaf_2006_c.txt
+./all_cpp_camp_uaf_2006_test.sh | tee camp_uaf_2006_cpp.txt
