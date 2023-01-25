ab189c1668c80b88de5c32805e29734b5d59653a
dataisland <dataisland@outlook.com>
2023-01-25 15:04:31 -0600

    Upload ffmalloc mt script

---
+++ records/spec/ffmalloc_mt/ffmalloc_mt.sh
@@ -0,0 +1,4 @@
+./int_ffmalloc_mt_2017_test.sh | tee ffmalloc_mt_2017_int.txt
+./fp_ffmalloc_mt_2017_test.sh | tee ffmalloc_mt_2017_fp.txt
+./all_c_ffmalloc_mt_2006_test.sh | tee ffmalloc_mt_2006_c.txt
+./all_cpp_ffmalloc_mt_2006_test.sh | tee ffmalloc_mt_2006_cpp.txt
