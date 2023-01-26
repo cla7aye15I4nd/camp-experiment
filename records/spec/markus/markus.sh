a002583cef89bd14abc74fdc744277e8124b0209
dataisland <dataisland@outlook.com>
2023-01-25 23:04:31 -0600

    Add markus script

---
+++ records/spec/markus/markus.sh
@@ -0,0 +1,4 @@
+./int_markus_2017_test.sh | tee markus_2017_int.txt
+./fp_markus_2017_test.sh | tee markus_2017_fp.txt
+./all_c_markus_2006_test.sh | tee markus_2006_c.txt
+./all_cpp_markus_2006_test.sh | tee markus_2006_cpp.txt
