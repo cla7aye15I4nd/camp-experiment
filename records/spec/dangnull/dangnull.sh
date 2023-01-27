9d8205ca71aaf8d8a74027bfd9f5075f8f01e7bd
dataisland <dataisland@outlook.com>
2023-01-27 12:09:23 -0600

    Upload dangnull result

---
+++ records/spec/dangnull/dangnull.sh
@@ -0,0 +1,4 @@
+./int_dangnull_2017_test.sh | tee dangnull_2017_int.txt
+./fp_dangnull_2017_test.sh | tee dangnull_2017_fp.txt
+./all_c_dangnull_2006_test.sh | tee dangnull_2006_c.txt
+./all_cpp_dangnull_2006_test.sh | tee dangnull_2006_cpp.txt
