62432e7323512192c3306500390b75690b5bca0a
dataisland <dataisland@outlook.com>
2023-01-27 12:15:30 -0600

    Upload partial lowfat result

---
+++ records/spec/lowfat/lowfat.sh
@@ -0,0 +1,4 @@
+./int_lowfat_2017_test.sh | tee lowfat_2017_int.txt
+./fp_lowfat_2017_test.sh | tee lowfat_2017_fp.txt
+./all_c_lowfat_2006_test.sh | tee lowfat_2006_c.txt
+./all_cpp_lowfat_2006_test.sh | tee lowfat_2006_cpp.txt
