3062e768e8b31b3391c389434a2b0d6e14d364bb
dataisland <dataisland@outlook.com>
2023-10-20 21:00:39 -0500

    Update build.sh

--- juliet/build.sh
+++ juliet/build.sh
@@ -9,12 +9,8 @@ if [ ! -d "dataset" ]; then
     cd dataset/testcases
     ls \
     | grep -v CWE122_Heap_Based_Buffer_Overflow \
-    | grep -v CWE124_Buffer_Underwrite \
-    | grep -v CWE126_Buffer_Overread \
-    | grep -v CWE127_Buffer_Underread \
     | grep -v CWE415_Double_Free \
     | grep -v CWE416_Use_After_Free \
-    | grep -v CWE476_NULL_Pointer_Dereference \
     | grep -v CWE761_Free_Pointer_Not_at_Start_of_Buffer \
     | xargs rm -rf
     cd -
