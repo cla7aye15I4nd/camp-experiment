8aad45209aa062a745e5b50b7578e694a5a80e9a
dataisland <dataisland@outlook.com>
2023-11-25 14:00:34 -0600

    Update statistic.py

--- spec/statistic.py
+++ spec/statistic.py
@@ -1,45 +1,86 @@
 import os
 import sys
 
-tag = 'native' if len(sys.argv) < 2 else sys.argv[1]
+def fetch_reslut(tag):
+    file_list = [
+        f'{tag}_2006_c.txt',
+        f'{tag}_2006_cpp.txt',
+        f'{tag}_2017_int.txt',
+        f'{tag}_2017_fp.txt',
+    ]
 
-file_list = [
-    f'{tag}_2006_c.txt',
-    f'{tag}_2006_cpp.txt',
-    f'{tag}_2017_int.txt',
-    f'{tag}_2017_fp.txt',
-]
+    result = []
+    last = None
+    for file in file_list:
+        with open(file) as f:
+            text = f.read().splitlines()
+            while text:
+                name = text.pop(0).split()[1].split('/')[2].split('_')[0]
+                if '2017' in file:
+                    name = name + '_s'
 
-result = []
-last = None
-for file in file_list:
-    with open(file) as f:
-        text = f.read().splitlines()
-        while text:
-            name = text.pop(0).split()[1].split('/')[2].split('_')[0]
-            if '2017' in file:
-                name = name + '_s'
+                time = int(text.pop(0).split()[2])
+                cputime = int(text.pop(0).split()[3])
+                memory = int(text.pop(0).split()[2])
+                exit = text.pop(0)
 
-            time = int(text.pop(0).split()[2])
-            cputime = int(text.pop(0).split()[3])
-            memory = int(text.pop(0).split()[2])
-            exit = text.pop(0)
+                if name == last:
+                    result[-1][1] += time
+                    result[-1][2] += cputime
+                    result[-1][3] += memory
+                else:
+                    last = name
+                    result.append([name, time, cputime, memory])
 
-            if name == last:
-                result[-1][1] += time
-                result[-1][2] += cputime
-                result[-1][3] += memory
-            else:
-                last = name
-                result.append([name, time, cputime, memory])
+    return result
 
-for item in result:
-    print(item[0], item[1]/1000, item[3])
+native = fetch_reslut('native')
+camp = fetch_reslut('camp')
 
-print('Mem')
-for item in result:
-    print(item[3])
-print('Time')
-for item in result:
-    print(item[1]/1000)
+print('CPU2017')
 
+time_geo_mean = 1
+mem_geo_mean = 1
+count = 0
+for i in range(len(native)):
+    name = native[i][0]
+    if not name.endswith('_s') or name == 'omnetpp_s': ## skip
+        continue
+    
+    count += 1
+        
+    time_overhead = (camp[i][1] - native[i][1]) / native[i][1] * 100
+    mem_overhead = (camp[i][3] - native[i][3]) / native[i][3] * 100
+    
+    print(f'{name}{" " * (12 - len(name))} {time_overhead:.2f}% \t{mem_overhead:.2f}%')
+    time_geo_mean *= time_overhead / 100
+    mem_geo_mean *= mem_overhead / 100 + 1
+    
+time_geo_mean = (time_geo_mean ** (1 / count)) * 100
+mem_geo_mean = (mem_geo_mean ** (1 / count) - 1) * 100
+
+print(f'Geomean      {time_geo_mean:.2f}% \t{mem_geo_mean:.2f}%')
+
+
+print('CPU2006')
+time_geo_mean = 1
+mem_geo_mean = 1
+count = 0
+for i in range(len(native)):
+    name = native[i][0]
+    if name.endswith('_s') or name == 'dealII': 
+        continue
+    
+    count += 1
+        
+    time_overhead = (camp[i][1] - native[i][1]) / native[i][1] * 100
+    mem_overhead = (camp[i][3] - native[i][3]) / native[i][3] * 100
+    
+    print(f'{name}{" " * (12 - len(name))} {time_overhead:.2f}% \t{mem_overhead:.2f}%')
+    time_geo_mean *= time_overhead / 100
+    mem_geo_mean *= mem_overhead / 100 + 1
+    
+time_geo_mean = (time_geo_mean ** (1 / count)) * 100
+mem_geo_mean = (mem_geo_mean ** (1 / count) - 1) * 100
+
+print(f'Geomean      {time_geo_mean:.2f}% \t{mem_geo_mean:.2f}%')
